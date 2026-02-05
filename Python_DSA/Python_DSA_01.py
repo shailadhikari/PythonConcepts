7739cc794a4c14637e43bb916e06d4d88c433497
Code Maverick <shailadhikari@gmail.com>
2026-02-04 22:09:48 -0500

    Updated Python_DSA_01 with new algo

--- Python_DSA/Python_DSA_01.py
+++ Python_DSA/Python_DSA_01.py
@@ -29,3 +29,27 @@ def insert_element_at_position(nums, element, position):
     nums.insert(position - 1, element)
     nums.pop()
     return nums
+
+
+#################################################################
+
+def get_intersection_with_maintained_frequency(a, b):
+    """
+    Args:
+     a(list_int32)
+     b(list_int32)
+    Returns:
+     list_int32
+    """
+    result = []
+    counts = {}
+
+    for x in a:
+        counts[x] = counts.get(x, 0) + 1
+
+    for x in b:
+        if counts.get(x, 0) > 0:
+            result.append(x)
+            counts[x] -= 1
+
+    return result
